da9f5f2948fb5a3731184fb58932ce2e3d5e3374
MBanja <MBanja@N-NO-01-04-8521>
2022-09-18 06:02:59 +0200

    fix: the function does not work, but at least the output translation does

--- react-sign-translator/src/views/Translation.jsx
+++ react-sign-translator/src/views/Translation.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react-hook-form"
 import TranslationInput from "../components/Translation/TranslationInput"
+import TranslationOutput from "../components/Translation/TranslationOutput"
 import withAuth from "../hoc/WithAuth"
 
 
 const Translation = () =>{
-
+    var imagetranslate = []
+    //const [inputText, setInputText] = useState()
     const handleTranslate = (text) =>{
         //Sjekk tekst lengde
         //Sett text i state?
@@ -13,6 +16,12 @@ const Translation = () =>{
         //Split text
         //Finn path
         //Print bilder i return i views, ikke output
+
+        /*
+        console.log(text)*/
+        imagetranslate = []
+        for (const c of text) {imagetranslate.push(c)}
+        
     }
 
     return(
@@ -22,6 +31,9 @@ const Translation = () =>{
             </section>
             <h3 className="translation_txt">Text to Sign Language:</h3>
 			{/* printe bilder her */}
+            {imagetranslate.map(c => (<TranslationOutput data={c} />))}
+            <TranslationOutput data={"a"} />
+
         </>
     )
 }
