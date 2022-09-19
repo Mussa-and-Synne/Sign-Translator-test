663b04ecc87d2c547f4c31c7ae37266521dc9c93
Synne Sævik <synne.savik@no.experis.com>
2022-09-19 14:08:27 +0200

    deploy: deployment try 2

--- react-sign-translator/server.js
+++ react-sign-translator/server.js
@@ -6,7 +6,7 @@ const packageJson = require('./package.json')
 app.use(requireHTTPS);
 app.use(express.static('./build/' + packageJson.name));
 
-// Redirect app request to build/index.html
+// Redirect app request to build/index.html!
 app.get('/*', (req, res) => {
   res.sendFile('index.html', {root: 'build/' + packageJson.name});
 });
